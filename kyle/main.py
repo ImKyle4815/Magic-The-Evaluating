8967c4b1134c758b61b5da8f2633f8632d4ec989
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2023-01-30 12:00:14 -0800

    Update main.py

--- kyle/main.py
+++ kyle/main.py
@@ -6,18 +6,17 @@ file = open("../dataset/oracle-cards.json")
 raw_cards = json.load(file)
 cards = []
 for raw_card in raw_cards:
-    if not ("name" in raw_card and "edhrec_rank" in raw_card and "oracle_text" in raw_card and "mana_cost" in raw_card and "type_line" in raw_card):
-        continue
-    else:
+    try:
         card = {}
         card["name"] = raw_card["name"]
         card["rules"] = raw_card["oracle_text"]
         card["cost"] = raw_card["mana_cost"]
         card["type"] = raw_card["type_line"]
-        card["rank"] = raw_card["edhrec_rank"]
+        card["rank"] = float(raw_card["edhrec_rank"])
+        card["usd"] = float(raw_card["prices"]["usd"])
         cards.append(card)
-
-cards = cards[:10000]
+    except:
+        continue
 
 ################################################################
 ################################################################
@@ -43,6 +42,10 @@ def extractValue(source, field):
     res = np.array(res)
     return res
 
+def normalizeValues(x):
+    x -= x.mean(axis=0)
+    x /= x.std(axis=0)
+    return x
 
 def maxLengthString(array):
     return len(max(array, key=len))
@@ -51,11 +54,17 @@ def maxLengthString(array):
 # Extract arrays
 vocab_size = 10000
 tokenizer = keras.preprocessing.text.Tokenizer(num_words=vocab_size)
+# Tokenize the text
 names = extractTokenized(cards, "name", tokenizer)
-rules = extractTokenized(cards, "rules", tokenizer)
 costs = extractTokenized(cards, "cost", tokenizer)
-types = extractValue(cards, "type")
-input_tensor = keras.layers.concatenate([names,costs,types,rules], axis=-1)
+types = extractTokenized(cards, "type", tokenizer)
+rules = extractTokenized(cards, "rules", tokenizer)
+print(names.shape, costs.shape, types.shape, rules.shape)
+input_tensor = np.concatenate([names, costs, types, rules], axis=-1)
+# Normalize training values
+# ranks = normalizeValues(extractValue(cards, "rank"))
+usd = extractValue(cards, "usd")
+# Get the max string length
 max_length = maxLengthString(input_tensor)
 
 # Building the model
@@ -63,11 +72,12 @@ model = keras.Sequential([
     keras.layers.Embedding(input_dim=vocab_size, output_dim=64, input_length=max_length),
     keras.layers.Flatten(),
     keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(1, activation='sigmoid')
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(1, activation='linear')
 ])
 
 # Compiling the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 # Training the model
-model.fit(input_tensor, ranks, epochs=10)
+model.fit(input_tensor, usd, epochs=10)
