e64944959b0264fe66a502d20a7cbfb114748101
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2023-02-03 11:33:37 -0800

    Update main.py

--- kyle/main.py
+++ kyle/main.py
@@ -1,22 +1,41 @@
 import json
+import math
 
 import numpy as np
+
+def transformPrice(price):
+    # return float(math.ceil(price))
+    return min(math.log10(price + 1) / 2, 1)
 
 file = open("../dataset/oracle-cards.json")
 raw_cards = json.load(file)
 cards = []
 for raw_card in raw_cards:
     try:
+        # Create a blank card
         card = {}
+        # Required fields (strings)
         card["name"] = raw_card["name"]
         card["rules"] = raw_card["oracle_text"]
         card["cost"] = raw_card["mana_cost"]
         card["type"] = raw_card["type_line"]
+        # Required fields (nums)
+        card["cmc"] = int(raw_card["cmc"])
+        # Optional fields
+        card["power"] = 0 if "power" not in raw_card else int(raw_card["power"])
+        card["toughness"] = 0 if "toughness" not in raw_card else int(raw_card["toughness"])
+        card["loyalty"] = 0 if "loyalty" not in raw_card else int(raw_card["loyalty"])
+        card["cmc"] = int(raw_card["cmc"])
+        # Evaluation labels
         card["rank"] = float(raw_card["edhrec_rank"])
-        card["usd"] = float(raw_card["prices"]["usd"])
+        card["usd"] = transformPrice(float(raw_card["prices"]["usd"]))
+        # print(card["usd"])
+        # Add the card to the list
         cards.append(card)
     except:
         continue
+# exit(0)
+
 
 ################################################################
 ################################################################
@@ -55,15 +74,18 @@ def maxLengthString(array):
 vocab_size = 10000
 tokenizer = keras.preprocessing.text.Tokenizer(num_words=vocab_size)
 # Tokenize the text
+print("Beginning to tokenize the text")
 names = extractTokenized(cards, "name", tokenizer)
 costs = extractTokenized(cards, "cost", tokenizer)
 types = extractTokenized(cards, "type", tokenizer)
 rules = extractTokenized(cards, "rules", tokenizer)
-print(names.shape, costs.shape, types.shape, rules.shape)
+print("Finished tokenizing the text")
+print("Final Shapes: ", names.shape, costs.shape, types.shape, rules.shape)
 input_tensor = np.concatenate([names, costs, types, rules], axis=-1)
 # Normalize training values
 # ranks = normalizeValues(extractValue(cards, "rank"))
 usd = extractValue(cards, "usd")
+# usd = normalizeValues(extractValue(cards, "usd"))
 # Get the max string length
 max_length = maxLengthString(input_tensor)
 
@@ -72,12 +94,12 @@ model = keras.Sequential([
     keras.layers.Embedding(input_dim=vocab_size, output_dim=64, input_length=max_length),
     keras.layers.Flatten(),
     keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(1, activation='linear')
+    keras.layers.Dense(16, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
 ])
 
 # Compiling the model
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
 
 # Training the model
-model.fit(input_tensor, usd, epochs=10)
+model.fit(input_tensor, usd, epochs=20)
