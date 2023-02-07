f462d7f46b04ee1203bf3c862be417c248358261
PhilippeWS <91435899+PhilippeWS@users.noreply.github.com>
2023-02-07 10:35:13 -0800

    Added Rules text cleaner method

--- Preproc.py
+++ Preproc.py
@@ -4,21 +4,46 @@ import os
 
 file = open("rawData\oracle-cards-2023_01_17.json", 'r', encoding='utf-8')
 raw_cards = json.load(file)
+
+
+def cleanRulesText(rules_text, card_name):
+    punctuation_marks = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    
+    for chararcter in punctuation_marks:
+        if chararcter in rules_text:
+            rules_text.replace(chararcter, "")
+            
+    if card_name in rules_text:
+        rules_text.replace(card_name, "CARDNAME")
+    
+    return rules_text.lower()
+
+
 cards = []
-for raw_card in raw_cards["cardlist"]:
+for raw_card in raw_cards:
     try:
+        # Create a blank card
         card = {}
+        # Required fields (strings)
         card["name"] = raw_card["name"]
-        card["rules"] = raw_card["oracle_text"]
-        card["cost"] = raw_card["mana_cost"]
+        card["rules"] = cleanRulesText(raw_card["oracle_text"], card["name"])
+        
+        
+        card["cost"] = raw_card["mana_cost"].replace("}{", " ").replace("/", "")[1:-2]
         card["type"] = raw_card["type_line"]
+        # Required fields (nums)
+        card["cmc"] = int(raw_card["cmc"])
+        # Optional fields
+        card["power"] = 0 if "power" not in raw_card else int(raw_card["power"])
+        card["toughness"] = 0 if "toughness" not in raw_card else int(raw_card["toughness"])
+        card["loyalty"] = 0 if "loyalty" not in raw_card else int(raw_card["loyalty"])
+        card["cmc"] = int(raw_card["cmc"])
+        # Evaluation labels
         card["rank"] = float(raw_card["edhrec_rank"])
-        card["usd"] = float(raw_card["prices"]["usd"])
         cards.append(card)
     except:
         continue
-
-
+    
 print(cards[0])
 file.close()
 ################################################################
