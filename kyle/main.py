bafbdf2c9c335dee46733bfc09c2c28c0e9dd8e3
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2023-02-13 11:40:42 -0800

    Update main.py

--- kyle/main.py
+++ kyle/main.py
@@ -4,8 +4,32 @@ import math
 import numpy as np
 
 def transformPrice(price):
-    # return float(math.ceil(price))
-    return min(math.log10(price + 1) / 2, 1)
+    if price < 0.17:
+        return 0
+    else:
+        return 1
+
+def categorizeType(type):
+    if "Creature" in type:
+        return 0
+    elif "Instant" in type:
+        return 0.2
+    elif "Sorcery" in type:
+        return 0.4
+    elif "Enchantment" in type:
+        return 0.6
+    elif "Artifact" in type:
+        return 0.8
+    elif "Land" in type:
+        return 1
+    else:
+        raise Exception("Ignoring type: " + type)
+
+def categorizeCMC(cmc):
+    if cmc < 10:
+        return cmc / 10
+    else:
+        return 10
 
 file = open("../dataset/oracle-cards.json")
 raw_cards = json.load(file)
@@ -18,14 +42,13 @@ for raw_card in raw_cards:
         card["name"] = raw_card["name"]
         card["rules"] = raw_card["oracle_text"].replace(card["name"], "CARDNAME")
         card["cost"] = raw_card["mana_cost"].replace("}{", " ").replace("/", "")[1:-2]
-        card["type"] = raw_card["type_line"]
+        card["type"] = categorizeType(raw_card["type_line"])
         # Required fields (nums)
-        card["cmc"] = int(raw_card["cmc"])
+        card["cmc"] = categorizeCMC(int(raw_card["cmc"]))
         # Optional fields
         card["power"] = 0 if "power" not in raw_card else int(raw_card["power"])
         card["toughness"] = 0 if "toughness" not in raw_card else int(raw_card["toughness"])
         card["loyalty"] = 0 if "loyalty" not in raw_card else int(raw_card["loyalty"])
-        card["cmc"] = int(raw_card["cmc"])
         # Evaluation labels
         card["rank"] = float(raw_card["edhrec_rank"])
         card["usd"] = transformPrice(float(raw_card["prices"]["usd"]))
@@ -78,37 +101,46 @@ tokenizer = keras.preprocessing.text.Tokenizer(num_words=vocab_size)
 # Tokenize the text
 print("Beginning to tokenize the text")
 rules = extractTokenized(cards, "rules", tokenizer)
-# names = extractTokenized(cards, "name", tokenizer)
-# names = np.pad(names, (0, rules.shape[1] - names.shape[1]))
-costs = extractTokenized(cards, "cost", tokenizer)
-# costs = np.pad(costs, (0, rules.shape[1] - names.shape[1]))
-types = extractTokenized(cards, "type", tokenizer)
-# types = np.pad(types, (0, rules.shape[1] - names.shape[1]))
-print("Finished tokenizing the text")
-# print("Shapes are: ", rules.shape, names.shape, costs.shape, types.shape)
-input_tensor = np.concatenate([costs, types, rules], axis=-1)
-# input_tensor = np.stack((names, costs, types, rules))
-print("Final Shape: ", input_tensor.shape)
-print(input_tensor[0])
-# Normalize training values
-# ranks = normalizeValues(extractValue(cards, "rank"))
+# costs = extractTokenized(cards, "cost", tokenizer)
+types = extractValue(cards, "type")
+cmc = extractValue(cards, "cmc")
+power = (extractValue(cards, "power"))
+toughness = (extractValue(cards, "toughness"))
+metadata = np.stack((cmc, power, toughness, types), axis=-1)
+print("Finished tokenizing the text.")
+# print(input_tensor[0])
+# Prepare training values (labels)
 usd = extractValue(cards, "usd")
-# usd = normalizeValues(extractValue(cards, "usd"))
-# Get the max string length
-max_length = maxLengthString(input_tensor)
+# Get the input shapes
+textShape = maxLengthString(rules)
+print(metadata.shape)
+metadataShape = metadata.shape[1]
+
 
 # Building the model
-model = keras.Sequential([
-    keras.layers.Embedding(input_dim=vocab_size, output_dim=64, input_length=max_length),
-    keras.layers.Conv1D(128, 8, activation='relu'),
-    # keras.layers.MaxPooling1D(2),
-    keras.layers.Flatten(),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(1, activation='sigmoid')
-])
+
+# text inputs
+textInputs = keras.Input(shape=(textShape,))
+# text processing layers
+embeddedText = keras.layers.Embedding(input_dim=vocab_size, output_dim=128, input_length=textShape)(textInputs)
+convLayer1 = keras.layers.Conv1D(128, 8, activation='relu')(embeddedText)
+maxPoolingLayer1 = keras.layers.MaxPooling1D(2)(convLayer1)
+convLayer2 = keras.layers.Conv1D(64, 8, activation='relu')(maxPoolingLayer1)
+maxPoolingLayer2 = keras.layers.MaxPooling1D(2)(convLayer2)
+flattenedText = keras.layers.Flatten()(maxPoolingLayer2)
+# metadata inputs
+metadataInputs = keras.Input(shape=(metadataShape,))
+# concatenate text with numeric metadata
+concatenatedLayers = keras.layers.concatenate([flattenedText, metadataInputs])
+# final processing
+denseLayer1 = keras.layers.Dense(16, activation='relu')(concatenatedLayers)
+# output layer
+outputLayer = keras.layers.Dense(1, activation='sigmoid')(denseLayer1)
+# construct the model
+model = keras.models.Model(inputs=[textInputs, metadataInputs], outputs=outputLayer)
 
 # Compiling the model
-model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Training the model
-model.fit(input_tensor, usd, epochs=12)
+model.fit([rules, metadata], usd, epochs=10)
