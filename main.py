77d5c4f7a28109cfb24fafc506ce8b26cf1d5a71
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2023-03-15 11:31:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import numpy as np
 from tensorflow import keras
 
 
+MAX_CARD_TEXT_LENGTH = 256
+METADATA_LENGTH = 15
+VOCAB_SIZE = 2048
+PRICE_CATEGORIES = [
+    0.05,
+    0.10,
+    0.15,
+    0.20,
+    0.25,
+    0.30,
+    0.50,
+    1.00,
+    10.00,
+    1000.00
+]
+
+
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -29,7 +46,6 @@ def get_text(card):
     text = text.replace(cardName, "cardname")
     text = text.replace(".", " . ")
     text = text.replace("\n", " \n ")
-    # text = text.sub(r'\s*\([^\(\)]*\)\s*', ' ', text)
     return text.lower()
 
 
@@ -62,36 +78,39 @@ def categorize_cmc(cmc):
         return 1
 
 
+def parse_raw_card(raw_card, price_cutoff_categories):
+    # Apply Filters
+    # if int(raw_card["released_at"][0:4]) < 2004:
+    #     raise Exception("Failed to parse raw card")
+    # Strings to be tokenized
+    card_text = get_text(raw_card)
+    # Concatenated Metadata
+    card_type = get_type(raw_card)
+    cmc = categorize_cmc(int(raw_card["cmc"]))
+    colors = get_colors(raw_card)
+    power = 0 if "power" not in raw_card else int(raw_card["power"]) / 10
+    toughness = 0 if "toughness" not in raw_card else int(raw_card["toughness"]) / 10
+    num_abilities = card_text.count('/n') / 10
+    card_metadata = card_type + colors + [cmc, num_abilities, power, toughness]
+    # Evaluation labels
+    card_price = float(raw_card["prices"]["usd"])
+    card_price_category = categorize_price(card_price, price_cutoff_categories)
+    # Add the values to the lists
+    return {
+        "name": raw_card["name"],
+        "text": card_text,
+        "metadata": card_metadata,
+        "price": card_price,
+        "price_category": card_price_category
+    }
+
 def parse_raw_cards(filepath, price_cutoff_categories):
     file = open(filepath)
     raw_cards = json.load(file)
     res = []
     for raw_card in raw_cards:
         try:
-            # Apply Filters
-            if int(raw_card["released_at"][0:4]) < 2004:
-                continue
-            # Strings to be tokenized
-            card_text = get_text(raw_card)
-            # Concatenated Metadata
-            card_type = get_type(raw_card)
-            cmc = categorize_cmc(int(raw_card["cmc"]))
-            colors = get_colors(raw_card)
-            power = 0 if "power" not in raw_card else int(raw_card["power"]) / 10
-            toughness = 0 if "toughness" not in raw_card else int(raw_card["toughness"]) / 10
-            num_abilities = card_text.count('/n') / 10
-            card_metadata = card_type + colors + [cmc, num_abilities, power, toughness]
-            # Evaluation labels
-            card_price = float(raw_card["prices"]["usd"])
-            card_price_category = categorize_price(card_price, price_cutoff_categories)
-            # Add the values to the lists
-            res.append({
-                "name": raw_card["name"],
-                "text": card_text,
-                "metadata": card_metadata,
-                "price": card_price,
-                "price_category": card_price_category
-            })
+            res.append(parse_raw_card(raw_card, price_cutoff_categories))
         except:
             continue
     return np.array(res)
@@ -104,60 +123,69 @@ def split_test_data(data=np.array([]), split=0.8):
 
     return data[:split_index], data[split_index + 1:]
 
+
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
 
 
-def train_categorization_model(card_texts, card_texts_vocab_size, card_metadata, card_prices, num_output_categories):
-    # PREPARING VALUES
-    text_shape = len(max(card_texts, key=len))
-    metadata_shape = card_metadata.shape[1]
+def get_categorization_model(card_texts_shape, card_texts_vocab_size, card_metadata_shape, num_output_categories):
     # BUILDING THE MODEL
     # TEXT PATH
     # text inputs
-    text_path_inputs = keras.Input(shape=(text_shape,))
+    text_path_inputs = keras.Input(shape=(card_texts_shape,))
     # text processing layers
     text_path_layer = keras.layers.Embedding(input_dim=card_texts_vocab_size, output_dim=128,
-                                             input_length=text_shape)(text_path_inputs)
+                                             input_length=card_texts_shape)(text_path_inputs)
     text_path_layer = keras.layers.Conv1D(128, 8, activation='relu')(text_path_layer)
     text_path_layer = keras.layers.MaxPooling1D(2)(text_path_layer)
     text_path_layer = keras.layers.Conv1D(64, 8, activation='relu')(text_path_layer)
     text_path_layer = keras.layers.MaxPooling1D(2)(text_path_layer)
     text_path_layer = keras.layers.Conv1D(32, 8, activation='relu')(text_path_layer)
     text_path_layer = keras.layers.MaxPooling1D(2)(text_path_layer)
+    text_path_layer = keras.layers.Conv1D(32, 8, activation='relu')(text_path_layer)
+    text_path_layer = keras.layers.MaxPooling1D(2)(text_path_layer)
     text_path_layer = keras.layers.Flatten()(text_path_layer)
     # METADATA PATH
     # metadata inputs
-    metadata_path_inputs = keras.Input(shape=(metadata_shape,))
+    metadata_path_inputs = keras.Input(shape=(card_metadata_shape,))
     # metadata processing layers
     metadata_path_layer = keras.layers.Dense(64, activation='relu')(metadata_path_inputs)
     # CONCAT PATH
     # concatenate text with numeric metadata
     concat_path_layer = keras.layers.concatenate([text_path_layer, metadata_path_layer])
     # final processing
+    concat_path_layer = keras.layers.Dense(32, activation='relu')(concat_path_layer)
     concat_path_layer = keras.layers.Dense(16, activation='relu')(concat_path_layer)
     # output layer
     concat_path_layer = keras.layers.Dense(num_output_categories, activation='softmax')(concat_path_layer)
     # construct the model
-    m = keras.models.Model(inputs=[text_path_inputs, metadata_path_inputs], outputs=concat_path_layer)
+    model = keras.models.Model(inputs=[text_path_inputs, metadata_path_inputs], outputs=concat_path_layer)
     # COMPILING THE MODEL
-    m.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    # DECLARING CALLBACKS
-    callbacks = [
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    # TRAINING THE MODEL
+    return model
+
+
+def get_callbacks():
+    return [
         keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=3),
         keras.callbacks.ModelCheckpoint(filepath="output/categorized_model/weights.pb", save_best_only=True,
                                         save_weights_only=True),
         keras.callbacks.TensorBoard(log_dir="output/logs")
     ]
-    # TRAINING THE MODEL
-    m.fit([card_texts, card_metadata], card_prices, epochs=128, validation_split=0.2, callbacks=callbacks)
-    return m
-
 
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
+
+
+def tokenize_texts(texts):
+    tokenizer = keras.preprocessing.text.Tokenizer(num_words=VOCAB_SIZE)
+    tokenizer.fit_on_texts(texts)
+    res = tokenizer.texts_to_sequences(texts)
+    res = keras.preprocessing.sequence.pad_sequences(res, maxlen=MAX_CARD_TEXT_LENGTH)
+    return res
 
 
 def print_price_categorization_stats(card_prices, cutoffs):
@@ -175,63 +203,44 @@ def extract_feature(array, key):
     return np.array(res)
 
 
-def tokenize_texts(source, text_tokenizer):
-    text_tokenizer.fit_on_texts(source)
-    res = text_tokenizer.texts_to_sequences(source)
-    res = keras.preprocessing.sequence.pad_sequences(res)
-    return res
-
-
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
 
-# Declare price categories
-price_cutoffs = [
-    0.05,
-    0.10,
-    0.15,
-    0.20,
-    0.25,
-    0.30,
-    0.50,
-    1.00,
-    10.00,
-    100.00
-]
 
-# Parse the raw cards and extract arrays of interest
-parsed_cards = parse_raw_cards("./input/oracle-cards-dataset.json", price_cutoffs)
-# Split out the test and train data
-train_cards, test_cards = split_test_data(parsed_cards)
-# Extract all the desired features
-train_categorized_prices = extract_feature(train_cards, "price_category")
-train_texts = extract_feature(train_cards, "text")
-train_metadata = extract_feature(train_cards, "metadata")
-test_categorized_prices = extract_feature(test_cards, "price_category")
-test_texts = extract_feature(test_cards, "text")
-test_metadata = extract_feature(test_cards, "metadata")
+def driver():
+    # Parse the raw cards and extract arrays of interest
+    parsed_cards = parse_raw_cards("./input/oracle-cards-dataset.json", PRICE_CATEGORIES)
+    # Split out the test and train data
+    train_cards, test_cards = split_test_data(parsed_cards)
+    # Extract all the desired features
+    train_categorized_prices = extract_feature(train_cards, "price_category")
+    train_texts = extract_feature(train_cards, "text")
+    train_metadata = extract_feature(train_cards, "metadata")
+    test_categorized_prices = extract_feature(test_cards, "price_category")
+    test_texts = extract_feature(test_cards, "text")
+    test_metadata = extract_feature(test_cards, "metadata")
 
+    # Print out the price categorization breakdowns
+    print_price_categorization_stats(train_categorized_prices, PRICE_CATEGORIES)
 
-# Print out the price categorization breakdowns
-print_price_categorization_stats(train_categorized_prices, price_cutoffs)
+    # Tokenize the rules text
+    train_tokenized_texts = tokenize_texts(train_texts)
 
-# Tokenize the rules text
-print("Beginning to tokenize the text")
-vocab_size = 2048
-tokenizer = keras.preprocessing.text.Tokenizer(num_words=vocab_size)
-train_tokenized_texts = tokenize_texts(train_texts, tokenizer)
-print("Finished tokenizing the text.")
+    # Train the network
+    model = get_categorization_model(MAX_CARD_TEXT_LENGTH, VOCAB_SIZE, METADATA_LENGTH, len(PRICE_CATEGORIES))
 
-# Train the network
-model = train_categorization_model(train_tokenized_texts, vocab_size, train_metadata, train_categorized_prices,
-                                   len(price_cutoffs))
+    model.fit([train_tokenized_texts, train_metadata], train_categorized_prices, epochs=10, validation_split=0.2,
+              callbacks=get_callbacks())
 
-model.evaluate([test_texts, test_metadata], test_categorized_prices)
+    model.evaluate([test_texts, test_metadata], test_categorized_prices)
 
-for test_card in test_cards[:10]:
-    print(test_card["name"], "is worth", test_card["price"], "under category", test_card["price_category"],
-          "and predicts as category", model.predict([np.array([test_card["text"]]), np.array([test_card["metadata"]])]))
+    for test_card in test_cards[:10]:
+        print(test_card["name"], "is worth", test_card["price"], "under category", test_card["price_category"],
+              "and predicts as category", model.predict([np.array([test_card["text"]]), np.array([test_card["metadata"]])]))
 
-# TO RUN THE TENSORBOARD WEB SERVER:
-# tensorboard --logdir ./output/logs
+    # TO RUN THE TENSORBOARD WEB SERVER:
+    # tensorboard --logdir ./output/logs
+
+if __name__ == "__main__":
+    driver()
